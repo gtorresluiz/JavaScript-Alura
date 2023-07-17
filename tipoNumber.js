95d74998691b325a7f66d562fedb217757f41ed8
Gabriel Torres <126830021+gtorresluiz@users.noreply.github.com>
2023-07-17 18:36:35 -0300

    Add files via upload

---
+++ tipoNumber.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacao = primeiroNumero + segundoNumero;
+
+console.log(operacao)
+
+// ponto flutuante
+
+const pontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / pontoFlutuante;
+
+console.log(novaOperacao)
+
+// NAN => Not a Number
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
