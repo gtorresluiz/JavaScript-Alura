eb66ea3afe564b0a1af98d862d258dd2e2f30f17
gtorresluiz <rm98600@fiap.com.br>
2023-08-08 19:11:13 -0300

    praticando

--- explicacoesIniciais/operadores/ternario.js
+++ explicacoesIniciais/operadores/ternario.js
@@ -9,3 +9,7 @@ const idadeCliente = 16;
 
             //condicao                  //true      /false
 console.log(idadeCliente >= idadeMin ? "cerveja" : "suco")
+
+//praticando
+numero = 10;
+console.log(numero % 2 == 0 ? "par" : "ímpar")
